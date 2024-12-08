bd4aa23da456b7d70762ddf955aa103ad9458537
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-08 14:52:01 -0600

    While Loop examples

---
+++ src/Loops/Exercises.java
@@ -0,0 +1,16 @@
+package Loops;
+
+public class Exercises {
+    public static void main(String[] args) {
+        int countdown = 10;
+
+        while (countdown > 0) {
+            System.out.println(countdown);
+            countdown--; // decrement operator
+        }
+
+        System.out.println("Happy new Year!!!");
+
+
+    }
+}
