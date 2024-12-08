30c35c12c896350eb3382da250d8a6c082123283
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-08 15:06:09 -0600

    For loop statement

---
+++ src/Loops/ForLoop.java
@@ -0,0 +1,25 @@
+package Loops;
+
+public class ForLoop {
+    public static void main(String[] args) {
+        // For Loop
+
+        // Structure
+
+//    for (statement 1; statement 2; statement 3) {
+//        // code block to be executed
+//    }
+
+        /**
+         * Statement 1: is executed one time before the execution of the code block
+         *
+         * statement 2: defines the condition for executing the code block
+         *
+         * statement 3: is executed every time after the code block has been executed
+         */
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println(i);
+        }
+    }
+}
