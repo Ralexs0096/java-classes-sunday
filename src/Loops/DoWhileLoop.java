01fe7c36554d99d9123cfc300c2ceaf5ccf32484
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-08 14:43:57 -0600

    do while - loop

---
+++ src/Loops/DoWhileLoop.java
@@ -0,0 +1,23 @@
+package Loops;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        // iterative = repetitive
+
+        // Structure (do = hacer)
+
+//        do {
+//          // code block to be executed
+//        }
+//        while (condition);
+
+        // next line of code
+
+        do {
+            System.out.println("Hello Infinitely");
+        }
+        while (false);
+
+        System.out.println("Next Line");
+    }
+}
