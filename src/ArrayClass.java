821be45d4b08d1bb5a100872720722c44d582457
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-09 18:57:36 -0600

    Create a basic array with multiple types

---
+++ src/ArrayClass.java
@@ -0,0 +1,29 @@
+public class ArrayClass {
+    public static void main(String[] args) {
+        // Array Structure
+        //
+        // type[] arrayName = {...values...}; (arrayName is plural)
+
+        // variable
+//        int num1 = 2;
+//        int num2 = 89;
+//        int num3 = 98;
+
+        int[] nums = { 2, 89, 98 };
+
+//        String car1 = "Volvo";
+//        String car2 = "BMW";
+//        String car3 = "Toyota";
+
+        // group - Array
+        String[] cars = {
+                "Volvo",
+                "BMW",
+                "Toyota",
+        };
+
+        double[] values = { 8.8, 9.9, 11.11 };
+
+        boolean[] booleanValues = { true, false, true, false };
+    }
+}
