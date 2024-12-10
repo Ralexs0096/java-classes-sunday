8a23c2459fc9a2c438235d826d82fe1a69b4d19e
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-09 19:06:56 -0600

    Access to the values using the index position

--- src/ArrayClass.java
+++ src/ArrayClass.java
@@ -1,29 +1,15 @@
 public class ArrayClass {
     public static void main(String[] args) {
-        // Array Structure
-        //
-        // type[] arrayName = {...values...}; (arrayName is plural)
-
-        // variable
-//        int num1 = 2;
-//        int num2 = 89;
-//        int num3 = 98;
-
         int[] nums = { 2, 89, 98 };
-
-//        String car1 = "Volvo";
-//        String car2 = "BMW";
-//        String car3 = "Toyota";
 
         // group - Array
         String[] cars = {
-                "Volvo",
-                "BMW",
-                "Toyota",
+                "Volvo", // 0
+                "BMW", // 1
+                "Toyota", // 2
         };
 
-        double[] values = { 8.8, 9.9, 11.11 };
-
-        boolean[] booleanValues = { true, false, true, false };
+        System.out.println(cars[0] + " " + cars[1] + " " + cars[2]);
+        System.out.println(nums[0] + " " + nums[1] + " " + nums[2]);
     }
 }
