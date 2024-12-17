d9b631a7c5f03d69d11e73272308eaa0858fdd78
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-16 19:42:01 -0600

    Array Exercises

---
+++ src/Exercises/Arrays.java
@@ -0,0 +1,26 @@
+package Exercises;
+
+public class Arrays {
+    public static void main(String[] args) {
+        /**
+         * Create and reverse an array of integers
+         *
+         * - declares an integer array of size 5
+         * - allows the user to input values for each position in the array
+         * - prints the values of the array in reverse order
+         */
+
+        // Your code here:
+
+
+        /**
+         * Calcutale the average of array values
+         *
+         * - Declares an array of size 10.
+         * - Fills the array with random values between 1 and 100.
+         * - Calculates and prints the average of the values in the array.
+         */
+
+        // Your code here:
+    }
+}
