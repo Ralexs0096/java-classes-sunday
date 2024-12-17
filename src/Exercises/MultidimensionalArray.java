6649bf224623905b3eb4809abddf524f2fd79e82
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-16 19:46:38 -0600

    Multidimensional array Exercises

---
+++ src/Exercises/MultidimensionalArray.java
@@ -0,0 +1,15 @@
+package Exercises;
+
+public class MultidimensionalArray {
+    public static void main(String[] args) {
+        /**
+         * Create a matrix (2D array) of size 3x3
+         *
+         * - Declares a matrix (2D array) of size 3x3.
+         * - Allows the user to input values for each position in the matrix.
+         * - Calculates and displays the sum of all the elements in the matrix.
+         */
+
+        // Your code here:
+    }
+}
