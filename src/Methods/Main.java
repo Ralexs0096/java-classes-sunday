4ddba1ef6f46cfbb74cf578c26e185ad53f8f7ef
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-23 19:29:23 -0600

    Multiple parameters

--- src/Methods/Main.java
+++ src/Methods/Main.java
@@ -1,13 +1,14 @@
 package Methods;
 
 public class Main {
-    static void sayHello(String fname) { // parameter
+    static void sayHello(String fname, int age) { // parameter
         // code to be executed
         System.out.println("Hello, " + fname + "!");
+        System.out.println("You are " + age);
     }
 
     public static void main(String[] args) {
-        sayHello("Antuan"); // argument
-        sayHello("Alex");
+        sayHello("Antuan", 14); // argument
+        sayHello("Alex", 28);
     }
 }
