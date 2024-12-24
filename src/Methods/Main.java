3884848aac0315cf33c8cc088edf37c708f0b668
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-23 19:36:40 -0600

    Return value types

--- src/Methods/Main.java
+++ src/Methods/Main.java
@@ -1,15 +1,21 @@
 package Methods;
 
 public class Main {
-    static void checkAge(int age) {
-        if(age < 18) {
-            System.out.println("Access denied - You are not old enough!");
-        } else {
-            System.out.println("Access granted - You are old enough!");
-        }
+    static int sum(int a, int b) {
+        return a + b;
+    }
+
+    static String concat(String text1, String text2){
+        return text1 + " " + text2;
     }
 
     public static void main(String[] args) {
-        checkAge(17);
+        System.out.println(sum(8, 8));
+
+        String fullName = concat("Antuan", "Narvaez");
+        System.out.println(fullName);
+
+        // another way to do the above example
+//        System.out.println(concat("Antuan", "Narvaez"));
     }
 }
