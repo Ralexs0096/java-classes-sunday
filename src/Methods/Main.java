ad99040712ae112532bae0261aac0e7eb2597ca2
Alexs Ruiz <ralexs.acu@gmail.com>
2024-12-23 19:23:33 -0600

    Define my first method

---
+++ src/Methods/Main.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class Main {
+    static void myMethod() {
+        // code to be executed
+        System.out.println("Hello!");
+    }
+
+    public static void main(String[] args) {
+        myMethod();
+        myMethod();
+    }
+}
